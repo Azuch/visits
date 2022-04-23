177d3b0b7560150aa61233809bb9baf589c23323
Azuch <namlucius@gmail.com>
2022-04-23 20:37:42 +0700

    add docker-compose.yaml

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+
+
+const client = redis.createClient({
+    host: "redis-server", /* Redis will try to find a url which is redis-server, when it try to connect to outside for this url, docker will catch and throw back the redis-server (service), which is define in docker-compose.yml */
+    port: 6379
+});
 
 client.set("visits", 0);
 
