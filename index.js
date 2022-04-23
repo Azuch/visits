2de6e4541e755f81314b1f8a098777b5f2b28354
Azuch <namlucius@gmail.com>
2022-04-23 20:43:28 +0700

    change redis image

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const client = redis.createClient({
     host: "redis-server", /* Redis will try to find a url which is redis-server, when it try to connect to outside for this url, docker will catch and throw back the redis-server (service), which is define in docker-compose.yml */
-    port: 6379
+    
 });
 
 client.set("visits", 0);
